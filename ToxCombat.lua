a7d02debe2a303bf3db21c3b3a5f3bc9b666d088
BG-0o <1koBG.0o@gmail.com>
2026-08-08 21:28:08 -0400

    Update ToxCombat.lua

--- ToxCombat.lua
+++ ToxCombat.lua
@@ -6,6 +6,7 @@ E.CreateToggle("Ctrl Click TP", FlingPage, Settings.CtrlClickTP, function(v) Set
 E.CreateToggle("No Fall Damage", FlingPage, Settings.NoFallDamage, function(v) Settings.NoFallDamage = v if v then E.StartNoFall() else E.StopNoFall() end end)
 E.CreateToggle("Anti Void", FlingPage, Settings.AntiVoid, function(v) Settings.AntiVoid = v if v then E.StartAntiVoid() else E.StopAntiVoid() end end)
 E.CreateToggle("Anti Fling", FlingPage, Settings.AntiFling, function(v) Settings.AntiFling = v if v then E.StartAntiFling() else E.StopAntiFling() end end)
+E.CreateToggle("Walk Fling", FlingPage, Settings.WalkFling, function(v) Settings.WalkFling = v if v then E.StartWalkFling() else E.StopWalkFling() end end)
 E.CreateInputWithButton("Target Fling", FlingPage, "", "Fling", function(text) E.ExecuteFling(text) end)
 
 E.CreateButton("Music Player", FlingPage, function()
